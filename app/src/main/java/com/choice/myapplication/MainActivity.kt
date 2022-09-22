278c4bd5ce1c3d3bf0e32e5c459ed69310add86d
F.zamora <f.zamora@namoadigital.com>
2022-09-22 10:51:01 -0300

    LIJTGH-6 :fire: changed message from action
    LIJTGH-6-criar-tela-de-financeiro
    
     @#$

--- app/src/main/java/com/choice/myapplication/MainActivity.kt
+++ app/src/main/java/com/choice/myapplication/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action Clicke", null).show()
+                .setAction("Action Clickeddddd", null).show()
         }
     }
 
